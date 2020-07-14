9a3e63f4af438271b3251c248d0aec71cf239181
Gábor Kovács <gabor950601@gmail.com>
2020-07-14 17:24:26 +0200

    Updates

---
+++ precache-manifest.53a288449bce52be1a8d9b3d54bec7de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ecb924d00dc7c7cb32020a031bca528",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80c90fc0c11a38fdcb28",
+    "url": "/static/css/main.01c11e55.chunk.css"
+  },
+  {
+    "revision": "48638204db5046aa607f",
+    "url": "/static/js/2.247b8878.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.247b8878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80c90fc0c11a38fdcb28",
+    "url": "/static/js/main.ea0e402d.chunk.js"
+  },
+  {
+    "revision": "becc1f554535d96f160c",
+    "url": "/static/js/runtime-main.d305fdd3.js"
+  },
+  {
+    "revision": "6652da12870b2c4d04b79c018dd70ece",
+    "url": "/static/media/aranylovas_kesz.6652da12.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wip-banner/precache-manifest.c8f85825308e5131ff327fa38ca6d0a4.js"
+  "/precache-manifest.53a288449bce52be1a8d9b3d54bec7de.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wip-banner/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
