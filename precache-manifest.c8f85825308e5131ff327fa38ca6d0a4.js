ddf20f4a545b9be022229d29b36fe20afea9409b
Gábor Kovács <gabor950601@gmail.com>
2020-07-14 15:40:47 +0200

    Updates

---
+++ precache-manifest.c8f85825308e5131ff327fa38ca6d0a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7d00bac9e562b7cf6f052caf52e19be",
+    "url": "/wip-banner/index.html"
+  },
+  {
+    "revision": "311f1b90e78e4a37fae5",
+    "url": "/wip-banner/static/css/main.01c11e55.chunk.css"
+  },
+  {
+    "revision": "48638204db5046aa607f",
+    "url": "/wip-banner/static/js/2.247b8878.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wip-banner/static/js/2.247b8878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "311f1b90e78e4a37fae5",
+    "url": "/wip-banner/static/js/main.a9827349.chunk.js"
+  },
+  {
+    "revision": "9c12731018ebb36c350a",
+    "url": "/wip-banner/static/js/runtime-main.b3cbd296.js"
+  },
+  {
+    "revision": "6652da12870b2c4d04b79c018dd70ece",
+    "url": "/wip-banner/static/media/aranylovas_kesz.6652da12.png"
+  }
+]);
