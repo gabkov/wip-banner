36f9dd6bf05a09b3d504ff5f2e1261c1c69bf362
Gábor Kovács <gabor950601@gmail.com>
2020-07-14 15:35:33 +0200

    add initial status setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './aranylovas_kesz.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <p>The site is under construction.</p>
+        <img src={logo} width="20%" height="20%" alt="logo" />
+        <p>Az oldal fejlesztés alatt.</p>
+        <p>Elérhetőség/Contact: info@aranylovas.hu</p>
+        <div id="wrapper">
+          <div id="first">
+            <a target="_blank" href="https://www.instagram.com/aranylo_vas/">
+              <img class="socialicon img-hover-zoom" src="http://monarchiaoperett.hu/images/instagram-2-icon-256.png"
+                height="40"
+                width="40"
+                alt="insta"
+              ></img>
+            </a>
+          </div>
+          <div id="second">
+            <a target="_blank" href="https://www.facebook.com/Aranyl%C3%B3-vas-104278354521849/">
+              <img class="socialicon img-hover-zoom" src="https://www.freepnglogos.com/uploads/facebook-icons/facebook-icon-transparent-background-3.png"
+                height="40"
+                width="40"
+                alt="face"
+              ></img>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
